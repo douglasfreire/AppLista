b821cab54a6c9c022e419d85c92534fbe8a7b558
douglasfreire <douglasfreire20@gmail.com>
2019-06-27 21:54:52 -0300

    Ordenação da lista e correção do bug no Snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,20 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      toDoList.sort((a, b){
+        if (a["ok"] && !b["ok"]) return 1;
+        else if (a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+      saveData();
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,11 +93,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
-                itemCount: toDoList.length,
-                itemBuilder: buildItem
-          ),
+            child: RefreshIndicator(onRefresh: refresh,
+                child: ListView.builder(
+                    padding: EdgeInsets.only(top: 10.0),
+                    itemCount: toDoList.length,
+                    itemBuilder: buildItem
+                )
+            )
           ),
         ],
       ),
@@ -133,6 +149,7 @@ class _HomeState extends State<Home> {
             }),
             duration: Duration(seconds: 3),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
